e386b6f8a4b36a39ab3b3966595baead750501b4
Amanda Murphy <helloamandamurphy@gmail.com>
2018-08-09 15:57:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(x)
+  if x % 3 == 0 #if the number x is divisible by 3
+  puts "Fizz"
+  elsif x % 5 == 0 #if the number x is divisible by 5
+  puts "Buzz"
+  elsif x % 3 % 5 == 0 #if the number x is divisible by 3 and 5
+  puts "FizzBuzz"
+else
+  puts "nil"
+end
+end
