7a4434478c781c77a347a10d5301421edc08d5ae
Amanda Murphy <helloamandamurphy@gmail.com>
2018-08-09 16:24:17 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(x)
-  if x % 3 == 0 #if the number x is divisible by 3
+  if x % 3 == 0 && x % 5 == 0 #if the number x is divisible by 3 and 5
+  "FizzBuzz"
+  elsif x % 3 == 0 #if the number x is divisible by 3
   "Fizz"
   elsif x % 5 == 0 #if the number x is divisible by 5
   "Buzz"
-  elsif x % 3 == 0 && x % 5 == 0 #if the number x is divisible by 3 and 5
-  "FizzBuzz"
 else
   nil
 end
